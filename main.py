98e727f2191f6537d73aa5aa24033a1aad724a53
BiggieFudge <73502334+BiggieFudge@users.noreply.github.com>
2022-06-30 18:52:55 +0300

    Class Classification

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import datetime
 import time
-
+from datetime import datetime
 import facebook_scraper
 import pandas as pd
 import hebrew_tokenizer as ht
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+import numpy as np
 
 
 #ML
@@ -41,6 +43,10 @@ def facebook_scrape():
                 "herzlia",#דירות להשכרה ושותפים בהרצליה
                  "HomeInHerzliyarent", #דירות להשכרה בהרצליה
                 "337487666800411", #דירות להשכרה ללא דמי תיווך -הרצליה,רעננה,כפר סבא,רמת השרון
+                "secretapartmentssharon",
+                "1909379842454834",
+                "344940935888684",
+                "dirotlehascarabeherzelia",
                 ]
     option = {
                 "progress": True,  "posts_per_page": 50, "allow_extra_requests": True,
@@ -66,7 +72,7 @@ def facebook_scrape():
     df = pd.DataFrame(temp)
     df.sort_values("time", inplace=True)
 
-    df.to_csv("facebook_scrape.csv", index=False, columns=columns, encoding="utf-8-sig")
+    df.to_csv(r"V2\facebook_scrape.csv", index=False, columns=columns, encoding="utf-8-sig")
 
 
 def tokenizer(str):
@@ -81,25 +87,44 @@ def tokenizer(str):
 
 
 
+def removeempty(str):
+    if type(str) is not float:
+        lines = str.split("\n")
+        non_empty_lines = [line for line in lines if line.strip() != ""]
+        string_without_empty_lines = ""
+
+        for line in non_empty_lines:
+            string_without_empty_lines += line
+        
+        return string_without_empty_lines
 
 
 
 def text_anal():
     info_columns = ["street","area","price","Rooms","Porch", "Garden"]
 
-    df = pd.read_csv("facebook_scrape.csv",encoding="utf-8-sig")
+    df = pd.read_csv(r"V2\facebook_scrape.csv",encoding="utf-8-sig")
     #df["text"] = df["text"].str.replace("\n", " ") IDK
+    df["text"] = df["text"].apply(removeempty)
 
     #delete unreleavent posts from group managers etc.
     df.drop(df[df["username"] == "פנטהאוזים להשכרה בהרצליה"].index, inplace=True)
     df.drop(df[df["username"] == "Eyal Amir"].index,inplace=True)
+    df.drop(df[df["username"] == "Ram Barak"].index, inplace=True)
     df.drop(df[df["username"] == "דירות ללא תיווך הרצליה, רמת השרון, רעננה"].index,inplace=True)
+    #df.drop(df[df["time"] > "1/1/2022  10:12:35 AM"].index, inplace=True)
+    df.drop_duplicates(subset=['text'], inplace=True)
+    df.drop_duplicates(subset=['username'], inplace=True)
+    df.dropna(subset="text", inplace=True)
+    #df.insert(len(df.columns), column="Rooms", value="")
 
-    df.insert(len(df.columns), column="Rooms", value="")
+    for index, row in df.iterrows():
+        if datetime.strptime(row["time"],'%Y-%m-%d %H:%M:%S') < datetime(2022,1,1,1,1,1):
+            df.drop(index=index)
 
     #Main for loop
-    for index, row in df.iterrows():
-        df.at[index,"Rooms"] = room_calc(tokenizer(row["text"]))
+    #for index, row in df.iterrows():
+        #df.at[index,"Rooms"] = room_calc(tokenizer(row["text"]))
         #TODO: add filtration to apartament for sale
         #TODO: add filtration to apartments that are from real easte
         #TODO: add apartment size
@@ -107,7 +132,7 @@ def text_anal():
         #TODO: remove duplicates by username
 
 
-    df.to_csv("facebook_scrape_clean.csv",encoding="utf-8-sig")
+    df.to_csv(r"V2\facebook_scrape_clean.csv",encoding="utf-8-sig")
 
 
 
@@ -130,26 +155,50 @@ def room_calc(liststr):
 
 def MachineLearn():
     #load CSV
-    X_df = pd.read_csv("facebook_scrape.csv",encoding="utf-8-sig")
+    X_df = pd.read_csv("facebook_scrape_clean.csv",encoding="utf-8-sig")
+    V2df = pd.read_csv(r"V2\facebook_scrape_clean.csv",encoding="utf-8-sig")
 
+    X_df.dropna(subset="text", inplace=True)
+    V2df.dropna(subset="text", inplace=True)
 
     #add relevent columns
-    Y_df = pd.DataFrame(columns=[["street","Area","price","Rooms","Class"]], index=range(X_df.shape[0]))
+    #Y_df = pd.DataFrame(columns=[["street","Area","price","Rooms","Class"]], index=range(X_df.shape[0]))
 
-    X_train, X_test, y_train, y_test = train_test_split(X_df['text'], Y_df['Class'], test_size=0.2,random_state=42)
+
+
+
+    #Class Classification
+    #S - Sale
+    #R - Rent
+    #Ro - Roomates
+    #M - Mediator
+    #N - Not relevent
+    X_train, X_test, y_train, y_test = train_test_split(X_df['text'], X_df['Class'], test_size=0.2,random_state=42)
+
 
     text_clf = Pipeline([('vect', TfidfVectorizer(tokenizer=tokenizer, max_features=100000)),
-                         ('CNN', MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(15, 4), random_state=1))])
+                         ('CNN', MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(15, 5), random_state=1))])
 
-    _ = text_clf.fit(X_train, y_train)
+    text_clf.fit(X_train, y_train)
 
-    pred_train = text_clf.predict(X_test)#TODO:create dataframe to predict on
+    #df_without["Class"].fillna("")
 
-    F1 = f1_score( X_test, y_test, average='macro')
 
-    print("F1 score is", F1)
 
+
+    prediction = text_clf.predict(X_test)
+    #print(pred_train)
+    F1 = f1_score(prediction, y_test, average='macro')
+
+    print("acc is", accuracy_score(y_test,prediction))
+
+    V2Pred= text_clf.predict(V2df["text"])
+
+    V2df["Class"] = V2Pred.tolist()
+
+    V2df.to_csv(r"V2\Prediction.csv",encoding="utf-8-sig")
 
 if __name__ == '__main__':
     #facebook_scrape()
     text_anal()
+    MachineLearn()
